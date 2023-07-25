610d4b40e5d33e2f5baff5a5b42609aaf9ea5437
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2023-07-25 15:38:15 -0400

    Add files via upload

--- LAME/settings.py
+++ LAME/settings.py
@@ -36,7 +36,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-
 s3 = boto3.resource('s3',
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key= AWS_SECRET_ACCESS_KEY,
@@ -66,8 +65,7 @@ CSRF_COOKIE_SECURE=False
 SESSION_COOKIE_SECURE=True
 SECURE_SSL_REDIRECT=False
 
-#ALLOWED_HOSTS = ['https://lame-b9e2e1e6b25e.herokuapp.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['3.208.228.10', 'www.lame.digital']
 
 # https://pythoncircle.com/post/697/hosting-django-app-for-free-on-amazon-aws-ec2-with-gunicorn-and-nginx/
 # server {
