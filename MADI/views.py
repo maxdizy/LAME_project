1eed4a4376dd4146031017cca4514445e82a853e
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2023-08-03 15:52:36 -0400

    Update views.py

--- MADI/views.py
+++ MADI/views.py
@@ -23,21 +23,19 @@ def upload(request):
     if request.method == 'POST':
         form = uploadForm(request.POST, request.FILES)
         #does ERFpath exist or is writable?
-        print(type(request.POST.get("ERFpath")))
-        if os.path.exists(request.POST.get("ERFpath")):
-            print("TRUE TRUE TRUE")
-            if form.is_valid():
-                form.save()
-            push_file('data/ERFL.txt', request.POST.get("ERFpath"))
-            global CN, tail, IRFTitle, description, affected, IRFNo, ROED, potROEDs, dart, mod, file
-            CN, file, dart, mod = request.POST.get('caseNo'), request.FILES.get('file'), request.POST.get("dart"), request.POST.get("mod")
-            tail, IRFTitle, description, affected, IRFNo, ROED, potROEDs = readIRF(file, CN)
-            if ROED:
-                return render(request, 'MADI/ROED.html', {'potROEDs' : potROEDs, 'hook' : hook, 'punch' : punch})
-            else:  
-                writeERF(CN, tail, IRFTitle, description, affected, IRFNo, ROED, False, potROEDs, dart, mod, file.name)
-        else:
-            return render(request, 'MADI/home.html', {'form' : uploadForm, 'hook' : hook, 'punch' : punch, 'warning' : 'ERROR: Some fields were not filled out correctly. Please make sure your ERF path is valid.'})
+        #if os.path.exists(request.POST.get("ERFpath")):
+        if form.is_valid():
+            form.save()
+        push_file('data/ERFL.txt', request.POST.get("ERFpath"))
+        global CN, tail, IRFTitle, description, affected, IRFNo, ROED, potROEDs, dart, mod, file
+        CN, file, dart, mod = request.POST.get('caseNo'), request.FILES.get('file'), request.POST.get("dart"), request.POST.get("mod")
+        tail, IRFTitle, description, affected, IRFNo, ROED, potROEDs = readIRF(file, CN)
+        if ROED:
+            return render(request, 'MADI/ROED.html', {'potROEDs' : potROEDs, 'hook' : hook, 'punch' : punch})
+        else:  
+            writeERF(CN, tail, IRFTitle, description, affected, IRFNo, ROED, False, potROEDs, dart, mod, file.name)
+        #else:
+            #return render(request, 'MADI/home.html', {'form' : uploadForm, 'hook' : hook, 'punch' : punch, 'warning' : 'ERROR: Some fields were not filled out correctly. Please make sure your ERF path is valid.'})
     return redirect('HOME-home')
 
 @login_required
