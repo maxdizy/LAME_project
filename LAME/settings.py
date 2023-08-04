e77391a36a27a1ebe1923f532a7cf68d6bc7600b
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2023-08-04 14:32:44 -0400

    Add files via upload

--- LAME/settings.py
+++ LAME/settings.py
@@ -102,25 +102,16 @@ WSGI_APPLICATION = 'LAME.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'LAMEDB',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRESQL_PASS'],
+        'HOST': 'lame-database.cdeddgxd1mqt.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
-
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'LAMEDB',
-            'USER': 'postgres',
-            'PASSWORD': os.environ['POSTGRESQL_PASS'],
-            'HOST': 'lame-database.cdeddgxd1mqt.us-east-1.rds.amazonaws.com',
-            'PORT': '5432',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
