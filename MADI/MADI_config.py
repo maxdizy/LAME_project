ec7a94298e3009a93a48a9cf769bafbc7b177665
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2023-08-04 13:35:21 -0400

    Update MADI_config.py

--- MADI/MADI_config.py
+++ MADI/MADI_config.py
@@ -127,9 +127,9 @@ def writeERF(CN, AC, SD, D, PN, IRF, ROED, new_ROED_file, potROED, dart, mod, IR
         os.makedirs(folLoc)
     except:
         print("Folder already exists; Pushing to folder.")
-    filLoc = folLoc + '\\' + str(CN) + '-' + str(AC) + '-' + SD + '.docx'
+    filLoc = '\\var\\www\\LAME_project\\ERFs\\' + str(CN) + '-' + str(AC) + '-' + SD + '.docx'
     #push_file(filLoc, document.write())
-    document.write('/var/www/LAME_project/ERFs/')
+    document.write(filLoc)
     #os.startfile(filLoc)
 
     # #move IRF to folder
